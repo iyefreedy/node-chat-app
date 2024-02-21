9427da619f27595ab77ddd7504e4008823376b75
Muhammad Quraisy <quraisy@uai.ac.id>
2024-02-21 18:26:14 +0700

    update frontend

--- backend/index.js
+++ backend/index.js
@@ -9,29 +9,39 @@ const authRoutes = require("./src/routes/authRoutes");
 
 const app = express();
 const server = createServer(app);
+const cookieSession = require('cookie-session')
+const cors = require('cors')
 
 app.use(express.json());
+app.use(cors())
+app.use(cookieSession({
+    domain: "http://localhost:5173",
+    httpOnly: true,
+    keys: ["token"],
+    maxAge: 24 * 60 * 60 * 1000 // 24 hours
+}))
 
 app.get("/", (req, res) => {
-	return res.status(200).json({
-		message: "Chat App v0.1",
-	});
+
+    return res.status(200).json({
+        message: "Chat App v0.1",
+    });
 });
 
 app.use(authRoutes);
 app.use("/api", userRoutes);
 
 const start = async () => {
-	try {
-		await mongoose.connect("mongodb://127.0.0.1:27017/chat-app");
+    try {
+        await mongoose.connect("mongodb://127.0.0.1:27017/chat-app");
 
-		server.listen(3000, () => {
-			console.log(`Server running at http://localhost:3000`);
-		});
-	} catch (error) {
-		console.error(error);
-		process.exit(1);
-	}
+        server.listen(3000, () => {
+            console.log(`Server running at http://localhost:3000`);
+        });
+    } catch (error) {
+        console.error(error);
+        process.exit(1);
+    }
 };
 
 start();
--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -6,33 +6,35 @@ const User = require("../models/user");
 const signIn = async (req, res) => {
 	const { email, password } = req.body;
 
+	console.log(email, password)
+
 	const foundUser = await User.findOne({ email: email }).exec();
 	if (!foundUser || !(await foundUser.validatePassword(password))) {
-		return res.status(401).send("Invalid credential");
+		return res.status(401).json({
+			message: "Invalid credential"
+		});
 	}
 
-	let privateKey = ''
-
-	fs.readFile(
+	let privateKey = fs.readFileSync(
 		"./private.pem",
 		{ encoding: "utf-8" },
-		(err, data) => {
-			if (err) {
-				throw new Error("Error while read data from file")
-			}
 
-			privateKey = data;
-		}
 	);
-	console.log(privateKey);
 
-	// const token = jwt.sign(foundUser, fs.readFileSync("./private.pem"), {
-	// 	algorithm: "RS256",
-	// 	expiresIn: "2h",
-	// });
+	const payload = {
+		uid: foundUser._id.toString(),
+		name: foundUser.name,
+	}
+
+	const token = jwt.sign(payload, privateKey, {
+		algorithm: "RS256",
+		expiresIn: "15m",
+		audience: "http://localhost:5173",
+		issuer: "http://localhost:3000"
+	});
 
 	return res.status(200).json({
-		access_token: "token",
+		access_token: token,
 	});
 };
 
